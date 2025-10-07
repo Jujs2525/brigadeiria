7951c3dc861b071ff45a4cb1f8ccfdf215f3a7e6
CamillyBenato <camillybenato.dev2024@gmail.com>
2025-10-07 13:21:49 -0300

    Projeto

--- brigadeiria/produtos/models.py
+++ brigadeiria/produtos/models.py
@@ -10,6 +10,8 @@ class Produto(models.Model):
     nome = models.CharField(max_length=100)
     preco = models.DecimalField(max_digits=10, decimal_places=2)
     descricao = models.TextField()
+    categoria = models.ForeignKey(Categoria, related_name='produtos', on_delete=models.CASCADE)
+    imagem = models.ImageField(upload_to = 'produtos/', blank = True, null = True)
 
     def __str__(self):
         return self.nome
